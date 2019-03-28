972fd68560979624a2d1265efddaf70592973f2f
Jose Augusto <joseaugusto.881@outlook.com>
2019-03-28 16:32:16 -0300

    now packages work!

--- myfetch.py
+++ myfetch.py
@@ -15,10 +15,16 @@ class Main:
         hostname = socket.gethostname()
         kernel = platform.uname()[2]
 
-        self.show_info(system, user, hostname, architecture, kernel)
+        path = "/var/lib/pacman/local/"
+        count = 0
+        for f in os.listdir(path):
+            count += 1
+        total_count = count - 1
+
+        self.show_info(system, user, hostname, architecture, kernel, total_count)
 
     @staticmethod
-    def show_info(system, user, hostname, architecture, kernel):
+    def show_info(system, user, hostname, architecture, kernel, total_count):
         infos = """
              {username}@{hostname}
            -----------------------
@@ -26,7 +32,7 @@ class Main:
     Kernel      : {kernel}
     Host        :
     DE          :
-    Packages    :
+    Packages    : {packages}
     Shell       :
     CPU         :
     GPU         :
@@ -35,7 +41,9 @@ class Main:
                hostname=hostname,
                sys=system,
                architecture=architecture,
-               kernel=kernel)
+               kernel=kernel,
+               packages=total_count
+               )
 
         print(infos)
 
