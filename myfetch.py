35396e83fe2bf4a309f0617da73e7f76eaf68e52
Jose Augusto <joseaugusto.881@outlook.com>
2019-03-28 21:54:29 -0300

    new information added

--- myfetch.py
+++ myfetch.py
@@ -21,28 +21,68 @@ class Main:
             count += 1
         total_count = count - 1
 
-        self.show_info(system, user, hostname, architecture, kernel, total_count)
+        # get host name
+        file = open("/sys/class/dmi/id/product_family", "r")
+        manufacture = file.read().strip()
+
+        # get DE or WM i think...
+        desktop = os.environ.get('DESKTOP_SESSION')
+
+        # getting current shell
+        shell = os.environ['SHELL']
+        current_shell = shell[5:]
+
+        # getting cpu name
+        with open('/proc/cpuinfo') as file:
+            for line in file:
+                if line.strip():
+                    if line.rstrip('\n').startswith('model name'):
+                        model_name = line.rstrip('\n').split(':')[1]
+                        break
+
+        self.show_info(system,
+                       user,
+                       hostname,
+                       architecture,
+                       kernel,
+                       total_count,
+                       manufacture,
+                       desktop,
+                       current_shell,
+                       model_name
+                       )
 
     @staticmethod
-    def show_info(system, user, hostname, architecture, kernel, total_count):
+    def show_info(system,
+                  user,
+                  hostname,
+                  architecture,
+                  kernel,
+                  total_count,
+                  manufacture,
+                  desktop,
+                  current_shell,
+                  model_name):
         infos = """
              {username}@{hostname}
            -----------------------
     OS          : {sys} {architecture}
     Kernel      : {kernel}
-    Host        :
-    DE          :
+    Host        : {manufacture}
+    DE          : {desktop}
     Packages    : {packages}
-    Shell       :
-    CPU         :
-    GPU         :
-    Memory      :
+    Shell       : {shell}
+    CPU         : {model}
     """.format(username=user,
                hostname=hostname,
                sys=system,
                architecture=architecture,
                kernel=kernel,
-               packages=total_count
+               packages=total_count,
+               manufacture=manufacture,
+               desktop=desktop,
+               shell=current_shell,
+               model=model_name.strip()
                )
 
         print(infos)
